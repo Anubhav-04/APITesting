209f297944e4d493fd4e997ee269101dff22e5e0
Anubhav-04 <69071614+Anubhav-04@users.noreply.github.com>
2025-08-13 19:36:15 +0530

    Added codes

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let todos = [];
+
+// GET all todos
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+// ADD a todo
+app.post("/todos", (req, res) => {
+  const todo = { id: todos.length + 1, task: req.body.task };
+  todos.push(todo);
+  res.status(201).json(todo);
+});
+
+// DELETE a todo
+app.delete("/todos/:id", (req, res) => {
+  todos = todos.filter(t => t.id !== parseInt(req.params.id));
+  res.status(204).send();
+});
+
+app.listen(3090, () => console.log("API running on http://localhost:3090"));
